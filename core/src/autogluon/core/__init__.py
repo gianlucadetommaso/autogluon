0bfb0592968fda802e52724a52be4517a56fe7b0
Innixma <neerick@amazon.com>
2023-02-09 19:19:33 -0800

    Move `_add_stream_handler()` call from core to common init

--- core/src/autogluon/core/__init__.py
+++ core/src/autogluon/core/__init__.py
@@ -1,11 +1,6 @@
-from autogluon.common.utils.log_utils import _add_stream_handler 
 
 from .dataset import TabularDataset
 from .space import Space, Categorical, Real, Int, Bool
 from . import metrics
 from . import constants
-
-
-_add_stream_handler()
-
 from .version import __version__
